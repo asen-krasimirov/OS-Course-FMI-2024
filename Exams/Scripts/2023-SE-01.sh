aa07d46268446d68e1385abac6a1f9c97a646977
asen-krasimirov <personalgrowth57@gmail.com>
2024-04-09 11:33:57 +0300

    adding better solution to 2023-SE-01

--- Exams/Scripts/2023-SE-01.sh
+++ Exams/Scripts/2023-SE-01.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-[[ $# == 2 ]] || { echo '2 args should be given'; exit 1; }
+[[ $# == 2 ]] || { echo 'script should take 2 args' >&2; exit 1; }
 
-[[ -f $1 ]] || { echo '1st arg should be a text file'; exit 2; }
+[[ -f "$1" ]] || { echo '1st arg should be a file' >&2; exit 2; }
 
-[[ -d $2 ]] || { echo '2st arg should be a dir'; exit 3; }
+[[ -d "$2" ]] || { echo '2nd arg should be a dir' >&2; exit 3; }
 
-while read file; do
-        while read badWord; do
-                astericsCount="$(echo "${badWord}" | wc -m)"
+while read word; do
+        replace="$(echo "$word" | tr '[a-zA-Z0-9_]' '*')"
 
-                toReplace="$(echo '*********************************' | grep -E -o "^\*{${astericsCount}}")" # Probably there is a better way to parse the asterics needed.
+        while read file; do
 
-                # This solutions asumes that the content of the file consists of 1 line.
-                line="$(cat ${file})"
-                # To support multi-line files another while-construction should be added.
+                sed -E -i "s:\<${word}\>:${replace}:gi" "$file"
 
-                echo "${line}" | sed -i -E "s/\ ${badWord}\ /\ ${toReplace}\ /" ${file} # Anchor is not working as expected ('\ ' should be replaced with '\<' and '\>').
-
-        done < <(cat $1)
-
-done < <(find . -type f -regex '.*\.txt$')
+        done < <(find "$2" -type f -name "*.txt")
+done < <(cat "$1")
